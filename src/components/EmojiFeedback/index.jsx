9e76c86e7938fb7bc55feb9662bcd1bfc73a57ba
Sanjay Soundarajan <sanjay.16@live.com>
2022-01-12 17:41:24 -0800

    Updated feeback

--- src/components/EmojiFeedback/index.jsx
+++ src/components/EmojiFeedback/index.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Emoji from "./Emoji";
-import { reactions } from "./data";
 
 const EmojiFeedback = ({ activeReaction, setActiveReaction }) => {
   const sendFeedback = (reaction) => {
@@ -9,35 +7,7 @@ const EmojiFeedback = ({ activeReaction, setActiveReaction }) => {
   };
 
   return (
-    <div className="">
-      <h4 className="text-xl text-black mt-4 mb-0">Was this page helpful?</h4>
-      <div className="flex flex-col relative">
-        <div className="flex flex-row justify-between ">
-          {reactions.map((reaction) => (
-            <Emoji
-              reaction={reaction}
-              key={reaction}
-              isSelected={activeReaction === reaction}
-              setActiveReaction={setActiveReaction}
-            />
-          ))}
-        </div>
-        <div className="flex justify-center w-full mt-2">
-          <button
-            className={
-              "text-xl font-bold rounded-md outline-none border-none px-10 py-2 text-white " +
-              (activeReaction !== ""
-                ? "cursor-pointer "
-                : "cursor-not-allowed ") +
-              (activeReaction !== "" ? "bg-black " : "bg-zinc-400")
-            }
-            onClick={() => sendFeedback(activeReaction)}
-          >
-            Submit
-          </button>
-        </div>
-      </div>
-    </div>
+    
   );
 };
 
--- src/components/PageFeedback.jsx
+++ src/components/PageFeedback.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { useState } from "react";
-import EmojiFeedback from "./EmojiFeedback";
+// import { useState } from "react";
 
 const PageFeedback = () => {
-  const [activeReaction, setActiveReaction] = useState("");
+  // const [activeReaction, setActiveReaction] = useState("");
   return (
-    <div className="flex justify-start items-center">
-      <EmojiFeedback
-        activeReaction={activeReaction}
-        setActiveReaction={setActiveReaction}
-      />
+    <div className="w-full mt-4 ">
+      <hr className="" />
+
+      <div className="flex flex-row items-center justify-start w-full mt-4">
+        <h3 className="pr-5 mb-0 w-max"> Was this page helpful? </h3>
+
+        <div className="flex items-center justify-center mt-2 space-x-4">
+          <button
+            className="feedbackButtonYes "
+            onClick={() => sendFeedback(activeReaction)}
+          >
+            Yes
+          </button>
+          <button
+            className="feedbackButtonNo"
+            onClick={() => sendFeedback(activeReaction)}
+          >
+            No
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
