97a1d06003af126847268429c43fbf046ad6da9a
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2022-01-12 21:15:02 -0800

    Added express app

--- src/components/PageFeedback.jsx
+++ src/components/PageFeedback.jsx
@@ -1,30 +1,133 @@
 import React from "react";
-// import { useState } from "react";
+import Lottie from "react-lottie";
+import ReactGA from "react-ga4";
+
+import LikeAnimationData from "./lotties/like.json";
+import DislikeAnimationData from "./lotties/dislike.json";
+import SucessAnimationData from "./lotties/success.json";
+
+const AskFeedback = ({ setShowSuccess }) => {
+  const [startLikeAnimation, setStartLikeAnimation] = React.useState(false);
+  const [startDislikeAnimation, setStartDislikeAnimation] =
+    React.useState(false);
+
+  const likeAnimationOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: LikeAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const dislikeAnimationOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: DislikeAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const sendFeedback = (reaction) => {
+    console.log("send feedback", reaction);
+    console.log(document.title);
+
+    const windowTitle = document.title;
+    const analyticsTitle = windowTitle.split(" | ")[0];
+
+    setShowSuccess(false);
+
+    // ReactGA.initialize([
+    //   {
+    //     trackingId: "G-T4726G4DJ0",
+    //     debug: true,
+    //   },
+    // ]);
+
+    // ReactGA.event({
+    //   category: analyticsTitle,
+    //   action: reaction,
+    //   transport: "beacon",
+    // });
+  };
+
+  return (
+    <div className="flex flex-row items-center justify-center w-full mt-4">
+      <h3 className="pr-5 mb-0 w-max"> Was this page helpful? </h3>
+
+      <div className="flex items-center justify-center space-x-4">
+        <button
+          className="feedback-button feedback-button-yes"
+          onClick={() => sendFeedback("Like")}
+          onMouseEnter={() => setStartLikeAnimation(true)}
+          onMouseLeave={() => setStartLikeAnimation(false)}
+        >
+          <Lottie
+            options={likeAnimationOptions}
+            height={30}
+            width={30}
+            isClickToPauseDisabled
+            isStopped={!startLikeAnimation}
+          />
+          <span className="px-2">Yes</span>
+        </button>
+        <button
+          className="feedback-button feedback-button-no"
+          onClick={() => sendFeedback("Dislike")}
+          onMouseEnter={() => setStartDislikeAnimation(true)}
+          onMouseLeave={() => setStartDislikeAnimation(false)}
+        >
+          <Lottie
+            options={dislikeAnimationOptions}
+            height={30}
+            width={30}
+            isClickToPauseDisabled
+            isStopped={!startDislikeAnimation}
+          />
+          <span className="px-2">No</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const ShowSuccessMessage = () => {
+  const animationOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: SucessAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className="flex flex-row items-center justify-center">
+      <h3 className="mb-0 w-max"> Thank you for your feedback! </h3>
+      <Lottie
+        options={animationOptions}
+        height={70}
+        width={70}
+        isClickToPauseDisabled
+        style={{ margin: "0" }}
+      />
+    </div>
+  );
+};
 
 const PageFeedback = () => {
-  // const [activeReaction, setActiveReaction] = useState("");
+  const [showSuccess, setShowSuccess] = React.useState(false);
+
   return (
     <div className="w-full mt-4 ">
-      <hr className="" />
+      <hr className="feedback-divider" />
 
-      <div className="flex flex-row items-center justify-start w-full mt-4">
-        <h3 className="pr-5 mb-0 w-max"> Was this page helpful? </h3>
-
-        <div className="flex items-center justify-center mt-2 space-x-4">
-          <button
-            className="feedbackButtonYes "
-            onClick={() => sendFeedback(activeReaction)}
-          >
-            Yes
-          </button>
-          <button
-            className="feedbackButtonNo"
-            onClick={() => sendFeedback(activeReaction)}
-          >
-            No
-          </button>
-        </div>
-      </div>
+      {showSuccess ? (
+        <ShowSuccessMessage />
+      ) : (
+        <AskFeedback setShowSuccess={setShowSuccess} />
+      )}
     </div>
   );
 };
