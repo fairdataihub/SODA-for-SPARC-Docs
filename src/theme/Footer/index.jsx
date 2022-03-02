73c1e20ca6787a1b2f50aa4a6a834738efa9ba41
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2022-03-01 22:02:39 -0800

    Add external link SVGs

--- src/theme/Footer/index.jsx
+++ src/theme/Footer/index.jsx
@@ -4,6 +4,23 @@ import Lottie from 'react-lottie';
 import CookiesLottieJSON from './cookies.json';
 import { useCookies } from 'react-cookie';
 
+export function ExternalLinkSVG() {
+  return (
+    <svg
+      width="13.5"
+      height="13.5"
+      aria-hidden="true"
+      viewBox="0 0 24 24"
+      class="iconExternalLink_node_modules-@docusaurus-theme-classic-lib-next-theme-IconExternalLink-styles-module"
+    >
+      <path
+        fill="currentColor"
+        d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"
+      ></path>
+    </svg>
+  );
+}
+
 export default function FooterWrapper() {
   const animationOptions = {
     loop: true,
@@ -408,12 +425,18 @@ export default function FooterWrapper() {
                   <ul className="m-0 list-outside list-none p-0">
                     <li className="pb-2 text-base ">
                       <div className="h-max w-max scale-100  transition-all hover:scale-105">
-                        <Link href="https://fairdataihub.org/team"> About </Link>
+                        <Link href="https://fairdataihub.org/team">
+                          {' '}
+                          About <ExternalLinkSVG />{' '}
+                        </Link>
                       </div>
                     </li>
                     <li className="pb-2 text-base ">
                       <div className="h-max w-max scale-100  transition-all hover:scale-105">
-                        <Link href="https://fairdataihub.org/contact-us">Contact Us</Link>
+                        <Link href="https://fairdataihub.org/contact-us">
+                          {' '}
+                          Contact Us <ExternalLinkSVG />
+                        </Link>
                       </div>
                     </li>
                   </ul>
