a05cfeb723d8a08438a65fea9e797ef597187f51
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2022-03-01 22:24:48 -0800

    Update linter

--- api/index.js
+++ api/index.js
@@ -1,37 +1,33 @@
-const express = require("express");
-var ua = require("universal-analytics");
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+let ua = require('universal-analytics');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/api/feedback", (req, res) => {
+app.post('/api/feedback', (req, res) => {
   const visitor = ua(process.env.UNIVERSAL_ANALYTICS_ID, uuidv4());
 
-  const body = req.body;
+  const { body } = req;
 
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
 
   if (body === undefined) {
     res.status(400).json({ message: `Invalid Request ${req.body}` });
+  } else if (body.category === undefined) {
+    res.status(400).json({ message: `Category Missing ${body}` });
+  } else if (body.action === undefined) {
+    res.status(400).json({ message: `Action Missing ${body}` });
   } else {
-    if (body.category === undefined) {
-      res.status(400).json({ message: `Category Missing ${body}` });
-    } else if (body.action === undefined) {
-      res.status(400).json({ message: `Action Missing ${body}` });
-    } else {
-      const category = body.category;
-      const action = body.action;
+    const { category, action } = body;
 
-      console.log(
-        `Feedback submitted for Category '${category}' with Action '${action}'`
-      );
+    // eslint-disable-next-line no-console
+    console.log(`Feedback submitted for Category '${category}' with Action '${action}'`);
 
-      visitor.event(category, action).send();
-      res.json({ success: true, message: `Feedback Sent` });
-    }
+    visitor.event(category, action).send();
+    res.json({ success: true, message: `Feedback Sent` });
   }
 });
 
