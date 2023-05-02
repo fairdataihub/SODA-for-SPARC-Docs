87c985e0fcff5e760041b30cd9f18ca64aaf5c07
Sanjay Soundarajan <sanjay.16@live.com>
2023-05-01 17:31:28 -0700

    chore: 🔨 update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,4 +3,8 @@ const { commitlintConfig } = require('@fairdataihub/config');
 
 const config = commitlintConfig();
 
+const scopes = ['app', 'library', 'backend', 'authentication', 'settings', 'deps', 'release'];
+
+config.rules['scope-enum'] = [2, 'always', scopes];
+
 module.exports = config;
