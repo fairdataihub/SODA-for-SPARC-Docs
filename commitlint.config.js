3f6753fe1d506a4aa94f8e895cb35104d599ac64
Sanjay Soundarajan <sanjay.16@live.com>
2023-05-01 17:54:20 -0700

    chore: 🔨 update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,5 +6,7 @@ const config = commitlintConfig();
 const scopes = ['app', 'library', 'backend', 'authentication', 'settings', 'deps', 'release'];
 
 config.rules['scope-enum'] = [2, 'always', scopes];
+config.rules['body-max-line-length'] = [2, 'always', 99999999999];
+config.rules['footer-max-line-length'] = [2, 'always', 99999999999];
 
 module.exports = config;
